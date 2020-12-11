4904e14d7c629478729b162cfe60d192edafac41
TonyTuCode <74125492+TonyTuCode@users.noreply.github.com>
2020-12-11 15:05:30 +0800

    v1.2

--- src/com/member/model/MemberService.java
+++ src/com/member/model/MemberService.java
@@ -2,26 +2,100 @@ package com.member.model;
 
 import java.util.List;
 import java.util.Set;
-import com.member.model.MemberVO;
+
 
 public class MemberService {
-	
+
 	private MemberDAO_interface dao;
-	
+
 	public MemberService() {
 		dao = new MemberJDBCDAO();
 	}
-	public List<MemberVO> getAll(){
-		return dao.getAll();
+
+	public MemberVO addMem(String m_email, String m_password, String m_name, String m_gender, String m_phone, String m_address,
+	 java.sql.Date m_birth,	 byte[] m_headpic, Integer m_status, String m_identity, byte[] m_id_pic, String m_account, String m_accountName,
+	 String b_code, byte[] m_bank_pic, String m_storename, String m_info, byte[] m_cover, String m_hi,
+	 String m_offlineHi, Integer m_coin) {
+		
+		MemberVO memberVO = new MemberVO();
+		
+		memberVO.setM_email(m_email);
+		memberVO.setM_password(m_password);
+		memberVO.setM_gender(m_gender);
+		memberVO.setM_name(m_name);
+		memberVO.setM_phone(m_phone);
+		memberVO.setM_address(m_address);
+		memberVO.setM_birth(m_birth);
+		memberVO.setM_headpic(m_headpic);
+		memberVO.setM_status(m_status);
+		memberVO.setM_identity(m_identity);
+		memberVO.setM_id_pic(m_id_pic);
+		memberVO.setM_account(m_account);
+		memberVO.setM_accountName(m_accountName);
+		memberVO.setB_code(b_code);
+		memberVO.setM_bank_pic(m_bank_pic);
+		memberVO.setM_storename(m_storename);
+		memberVO.setM_info(m_info);
+		memberVO.setM_cover(m_cover);
+		memberVO.setM_hi(m_hi);
+		memberVO.setM_offlineHi(m_offlineHi);
+		memberVO.setM_coin(m_coin);
+		
+		dao.insert(memberVO);
+		
+		return memberVO;
 	}
 	
-	public Set<MemberVO> getMemberByStatus(Integer m_status){
-		return dao.getMemberByStatus(m_status);
-	}	
+	public MemberVO updateMem(String m_id,String m_email, String m_password, String m_name, String m_gender, String m_phone, String m_address,
+			 java.sql.Date m_birth,	 byte[] m_headpic, Integer m_status, String m_identity, byte[] m_id_pic, String m_account, String m_accountName,
+			 String b_code, byte[] m_bank_pic, String m_storename, String m_info, byte[] m_cover, String m_hi,
+			 String m_offlineHi, Integer m_coin) {
+				
+				MemberVO memberVO = new MemberVO();
+				
+				memberVO.setM_id(m_id);
+				memberVO.setM_email(m_email);
+				memberVO.setM_password(m_password);
+				memberVO.setM_gender(m_gender);
+				memberVO.setM_name(m_name);
+				memberVO.setM_phone(m_phone);
+				memberVO.setM_address(m_address);
+				memberVO.setM_birth(m_birth);
+				memberVO.setM_headpic(m_headpic);
+				memberVO.setM_status(m_status);
+				memberVO.setM_identity(m_identity);
+				memberVO.setM_id_pic(m_id_pic);
+				memberVO.setM_account(m_account);
+				memberVO.setM_accountName(m_accountName);
+				memberVO.setB_code(b_code);
+				memberVO.setM_bank_pic(m_bank_pic);
+				memberVO.setM_storename(m_storename);
+				memberVO.setM_info(m_info);
+				memberVO.setM_cover(m_cover);
+				memberVO.setM_hi(m_hi);
+				memberVO.setM_offlineHi(m_offlineHi);
+				memberVO.setM_coin(m_coin);
+				
+				dao.update(memberVO);
+				
+				return memberVO;
 	
+	}
+
+	public void deleteMem(String m_id) {
+		dao.delete(m_id);
+	}
+
+	public List<MemberVO> getAll() {
+		return dao.getAll();
+	}
+
+	public Set<MemberVO> getMemberByStatus(Integer m_status) {
+		return dao.getMemberByStatus(m_status);
+	}
+
 	public MemberVO getMemberPw(String m_email) {
 		return dao.getMemberPw(m_email);
-		
 	}
 
 }
