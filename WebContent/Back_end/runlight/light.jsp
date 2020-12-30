fb734689d8f7443286c918fa84302177e779bed5
Jimmy Ting <dgiloveyou60@gmail.com>
2020-12-30 16:46:24 +0800

    22

--- WebContent/Back_end/runlight/light.jsp
+++ WebContent/Back_end/runlight/light.jsp
@@ -1,5 +1,18 @@
+<%@page import="java.util.Set"%>
+<%@page import="java.util.List"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@page import="redis.clients.jedis.Jedis"%>
 <%@ page language="java" contentType="text/html; charset=BIG5"
-    pageEncoding="BIG5"%>
+	pageEncoding="BIG5"%>
+<%
+Jedis jedis = new Jedis("localhost", 6379);
+jedis.auth("123456");
+
+
+
+    jedis.close();
+%>
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +20,10 @@
 <title>Insert title here</title>
 </head>
 <body>
- ${key};${value};
+
+
+<td></td>
+
+
 </body>
 </html>
--- src/com/runlight/controller/RunlightServlet.java
+++ src/com/runlight/controller/RunlightServlet.java
@@ -4,12 +4,13 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import redis.clients.jedis.Jedis;
-
+@WebServlet("/light.do")
 public class RunlightServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,13 +28,16 @@ public class RunlightServlet extends HttpServlet {
 		
 			Jedis jedis = new Jedis("localhost", 6379);
 			jedis.auth("123456");
-			System.out.println(jedis.ping());
+//			System.out.println(jedis.ping());
 			String key=req.getParameter("key");
-			System.out.println(key);
+//			System.out.println(key);
 			String value=req.getParameter("value");
-			System.out.println(value);
+//			System.out.println(value);
 			jedis.set(key,value);
 			
+			
+			
+          
 			String url = "/back_end/runlight/light.jsp";
 			RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllEmp.jsp
 			successView.forward(req, res);
@@ -44,25 +48,6 @@ public class RunlightServlet extends HttpServlet {
 		
 		
 		
-//	        String bookList = jedis.get("bookList");
-//	        if(null == bookList || "".equals(bookList)){
-////	          查詢資料庫
-//	            String mysqlDate="data";
-////	            將mysqlDate轉成json陣列串
-//	            jedis.set("bookList",mysqlDate);
-//	            bookList=  jedis.get("bookList");
-//	            req.setAttribute("msg","走了資料庫拿資料");
-//	            req.setAttribute("bookList",bookList);
-//	            req.getRequestDispatcher("/bookList.jsp").forward(req,resp);
-//	        }else{
-//	            req.setAttribute("msg","直接從redis中取得資料");
-//	            req.setAttribute("bookList",bookList);
-//	            req.getRequestDispatcher("/bookList.jsp").forward(req,resp);
-//	        }
-//	    }
-//		
-//		
-//		
 		}
 
 }
